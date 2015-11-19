9dcc2c5ac3e449d1d78c24dd09ee321a4c08d911
Peter Washington <peter.y.washington@gmail.com>
2015-11-18 21:17:13 -0800

    seeing which files were added

---
+++ Rubato/Rubato/ViewControllerSpotify.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerSpotify.h
+//  Rubato
+//
+//  Created by Peter Washington on 11/18/15.
+//  Copyright © 2015 Peter Washington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerSpotify : UIViewController
+
+@end
---
+++ Rubato/Rubato/ViewControllerSpotify.m
@@ -0,0 +1,37 @@
+//
+//  ViewControllerSpotify.m
+//  Rubato
+//
+//  Created by Peter Washington on 11/18/15.
+//  Copyright © 2015 Peter Washington. All rights reserved.
+//
+
+#import "ViewControllerSpotify.h"
+
+@interface ViewControllerSpotify ()
+
+@end
+
+@implementation ViewControllerSpotify
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
